b00c6dc8676af782f26712c055ddf47af0895625
carloscm2022 <carloscr7cmreal@gmail.com>
2022-11-27 23:52:07 -0500

    fix task

--- models/user.py
+++ models/user.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-'''
-    Implementation of the User class which inherits from BaseModel
-'''
-from os import getenv
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+import models
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
-    __tablename__ = "users"
-    if getenv("HBNB_TYPE_STORAGE", "fs") == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
+    """This class defines a user by various attributes"""
+    if models.storage_type == 'db':
+        tablename = "users"
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user",
-                              cascade="all, delete, delete-orphan")
-        reviews = relationship("Review", backref="user",
-                               cascade="all, delete, delete-orphan")
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user", cascade='all')
+        reviews = relationship("Review", backref="user", cascade='all')
     else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
