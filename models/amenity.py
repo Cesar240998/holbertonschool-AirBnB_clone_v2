12b7538d1326c4860f6479f4e14e0b72d1345ae0
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-06 15:48:00 -0500

    importing uuid

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 import models
 from models.base_model import BaseModel,Base
 from  models import storage
+from uuid import uuid4
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
